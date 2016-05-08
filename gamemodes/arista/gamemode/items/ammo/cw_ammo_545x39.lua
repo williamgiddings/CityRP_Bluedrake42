01c3036b611b987ab2bb3c8b4d39aef94872483e
williamgiddings <wgidz99@gmail.com>
2016-05-08 19:34:18 +0100

    Update cw_ammo_545x39.lua

--- gamemodes/arista/gamemode/items/ammo/cw_ammo_545x39.lua
+++ gamemodes/arista/gamemode/items/ammo/cw_ammo_545x39.lua
@@ -6,5 +6,5 @@ ITEM.batch				= 10
 ITEM.store				= true
 ITEM.plural				= "5.45x39MM Rounds"
 ITEM.description	= "Rounds for 39mm assualt rifles."
-ITEM.base					= "cw_ammo_ent_base"
+ITEM.base					= "ammo"
 ITEM.ammo					= {"5.45x39MM", 30}
